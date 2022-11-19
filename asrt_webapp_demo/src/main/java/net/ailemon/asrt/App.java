4dfaa57b3b2135f1f9bc4c0b5cdbc4e53f4f872f
ailemon <3210346136@qq.com>
2022-11-19 21:20:58 +0800

    修复maven的artifactId问题

---
+++ asrt_webapp_demo/src/main/java/net/ailemon/asrt/App.java
@@ -0,0 +1,13 @@
+package net.ailemon.asrt;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
